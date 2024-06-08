42740c5adb8df423feb78e25ebfa86df31595f03
shaokang.sk <shaokang.sk@bytedance.com>
2024-06-08 21:34:24 +0800

    first commit

--- water.go
+++ water.go
@@ -15,10 +15,10 @@ func NewTun(reqIP, respIP string) *water.Interface {
 	inst, err := water.New(water.Config{ // 构建一个网卡实例
 		DeviceType: water.TUN,
 	})
-	HandleErr(err)
+	HandleErr(err) // 对构建的网卡进行配置并启用，reqIP就是请求方的ip,respIP就是本地ip
 	cmd := exec.Command("ifconfig", inst.Name(), reqIP, respIP, "up")
 	err = cmd.Run()
-	HandleErr(err)
+	HandleErr(err) // 打印相关信息方便后续调试
 	fmt.Printf("init tun success name %v reqIP %s respIP %s\n", inst.Name(), reqIP, respIP)
 	return inst
 }
